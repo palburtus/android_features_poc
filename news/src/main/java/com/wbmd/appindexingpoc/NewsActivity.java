361d70cb12a16f51984b66cffbcecc23377f6682
alliejc <alison.jane.c@gmail.com>
2018-05-30 11:50:13 -0400

    adding showInstallPrompt to test user conversion

--- news/src/main/java/com/wbmd/appindexingpoc/NewsActivity.java
+++ news/src/main/java/com/wbmd/appindexingpoc/NewsActivity.java
@@ -2,9 +2,13 @@ package com.wbmd.appindexingpoc;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.instantapps.InstantApps;
 import com.google.firebase.appindexing.Action;
 import com.google.firebase.appindexing.FirebaseUserActions;
 import com.google.firebase.appindexing.builders.Actions;
@@ -16,17 +20,39 @@ import com.wbmd.appindexingpoc.news.R;
 
 public class NewsActivity extends Activity {
 
+    private Button mSeeArticleButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        Intent intent = getIntent();
-        String action = intent.getAction();
-        String data = intent.getDataString();
-        if (Intent.ACTION_VIEW.equals(action) && data != null) {
-            // TODO: Parse the data URL and show right content for the URL.
+        mSeeArticleButton = findViewById(R.id.see_article_button);
+        final Boolean isInstantApp = InstantApps.isInstantApp(this);
+        if(isInstantApp) {
+            mSeeArticleButton.setVisibility(View.VISIBLE);
+            mSeeArticleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    InstantApps.showInstallPrompt(NewsActivity.this, getPostInstallIntent(), 0, "instant");
+                }
+            });
+
+        } else {
+            //TODO: load article
+            mSeeArticleButton.setVisibility(View.GONE);
+            Intent intent = getIntent();
+            String action = intent.getAction();
+            String data = intent.getDataString();
+            if (Intent.ACTION_VIEW.equals(action) && data != null) {
+                // TODO: Parse the data URL and show right content for the URL.
+            }
         }
+    }
+
+    private Intent getPostInstallIntent(){
+        return new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.wbmd.appindexingpoc"))
+                .addCategory(Intent.CATEGORY_BROWSABLE);
     }
 
 //    /**
